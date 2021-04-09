ad9865893a7c38ff8ca12039858464760aa6ce9c
Leonardo Arango <rocker1058@gmail.com>
2021-04-09 16:35:12 -0500

    Notificaciones Arreglos

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:global_inv/src/pages/contador_page.dart';
 
+
 class HomePage extends StatelessWidget {
   @override
+  
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
